4cbc2ce0ffb951b9a8a470c36d12ac927cd017a8
deathbybandaid <sam@deathbybandaid.net>
2019-10-25 13:14:03 -0400

    test

--- sopel/modules/reddit.py
+++ sopel/modules/reddit.py
@@ -208,7 +208,7 @@ def auto_redditor_info(bot, trigger, match):
 
 @url(subreddit_url)
 def auto_subreddit_info(bot, trigger, match):
-    return bot.say(str(trigger))
+    return bot.say(str(trigger).split("/")[-1])
     subreddit_info(bot, trigger, match.group(2))
 
 
