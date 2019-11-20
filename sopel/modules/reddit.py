d2e303c3d458f9357807e75c3e19fff8eded48a3
deathbybandaid <sam@deathbybandaid.net>
2019-11-20 16:05:06 -0500

    test

--- sopel/modules/reddit.py
+++ sopel/modules/reddit.py
@@ -38,7 +38,7 @@ else:
 
 
 domain = r'https?://(?:www\.|old\.|pay\.|ssl\.|[a-z]{2}\.)?reddit\.com'
-subreddit_url = r'%s/r?/([\w-]+)' % domain
+subreddit_url = r'%s/r/([\w-]+)/?$' % domain
 post_url = r'%s/r/.*?/comments/([\w-]+)/?$' % domain
 short_post_url = r'https?://redd.it/([\w-]+)'
 user_url = r'%s/u(ser)?/([\w-]+)' % domain
