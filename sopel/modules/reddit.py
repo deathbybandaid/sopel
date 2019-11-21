826a73017c43d709d2a1ee638a1c0d4f969d6ece
deathbybandaid <sam@deathbybandaid.net>
2019-11-21 08:42:36 -0500

    test

--- sopel/modules/reddit.py
+++ sopel/modules/reddit.py
@@ -392,7 +392,6 @@ def get_channel_spoiler_free(bot, trigger):
 def reddit_slash_info(bot, trigger):
     searchtype = trigger.group('prefix')
     match = trigger.group('id')
-    bot.say(str(trigger.group(3)))
     if searchtype == "r":
         return subreddit_info(bot, trigger, match, commanded=True)
     elif searchtype == "u":
